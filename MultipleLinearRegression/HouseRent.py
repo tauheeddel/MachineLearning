e889fd4285f55c911cf0ef35c174aae88d66aafc
Tauheed Khan Mohd <tauheed.del@gmail.com>
2021-10-22 22:20:43 -0500

    Added code for cleaning of data

--- MultipleLinearRegression/HouseRent.py
+++ MultipleLinearRegression/HouseRent.py
@@ -1,10 +1,17 @@
 import pandas as pd
+from sklearn import preprocessing
 
 ### LOAD DATA ###
 data = pd.read_csv('houses_to_rent.csv', sep=',')
-data= data[['city', 'rooms', 'bathroom', 'parking spaces', 'fire insurance', 'furniture', 'rent amount']]
+data = data[['city', 'rooms', 'bathroom', 'parking spaces', 'fire insurance', 'furniture', 'rent amount']]
 
 print(data.head())
 
-
 #### PROCESS DATA #####
+data ['rent amount'] = data['rent amount'].map(lambda i: int(i[2:].replace(',','')))
+data ['fire insurance'] = data['fire insurance'].map(lambda i: int(i[2:].replace(',','')))
+
+le = preprocessing.LabelEncoder()
+data['furniture'] =  le.fit_transform((data['furniture']))
+
+print(data.head())
