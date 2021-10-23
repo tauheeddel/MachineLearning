42d8ee85bee461a03c72470192391395faebf479
Tauheed Khan Mohd <tauheed.del@gmail.com>
2021-10-22 20:52:32 -0500

    Initial commit

---
+++ SingleLinearRegression/LinearRegressionTemp.py
@@ -0,0 +1,15 @@
+import sklearn
+import matplotlib as plt
+import numpy as np
+
+# F = 1.8*C + 32
+# y = mx + c
+x = list(range(0, 39))   #C
+# List Comprehension is a for loop in one line
+y = [1.8*F + 32 for F in x]
+print(f'X: {x}')
+print(f'Y: {y}')
+
+
+plt.plot(x, y, '-*r')
+plt.show()
